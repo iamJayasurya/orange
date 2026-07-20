9fdc397ac856a84dd04dd3de28a950a14f747047
Jaya Surya <surya.appac@gmail.com>
2026-07-20 11:34:15 +0530

    Common button updated - SURYA

--- components/common/Buttons.tsx
+++ components/common/Buttons.tsx
@@ -9,11 +9,13 @@ type ButtonProps = {
 export default function Button({ link, label, variant = "white" }: ButtonProps) {
   const styles =
     variant === "white"
-      ? "bg-[#fff] text-[#000]"
-      : "bg-[#000] text-[#fff]"
+      ? "bg-[#fff] text-[#000] hover:bg-[#000] hover:text-[#fff] border-[#fff] hover:border-[#000]"
+      : "bg-[#000] text-[#fff] hover:bg-[#fff] hover:text-[#000]"
 
   return (
-    <Link href={link} className={`${styles} p-[15px_48px] uppercase text-[16px] leading-[20px] text-[500] `} aria-label={`${link} link to ${label}`}>
+    <Link href={link} className={`${styles} block w-fit       border-1 p-[15px_48px] rounded-[5px] transition-colors
+                        duration-300
+                         uppercase text-[16px] leading-[20px] text-[500] `} aria-label={`${link} link to ${label}`}>
       {label}
     </Link>
   )
